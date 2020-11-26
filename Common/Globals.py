f6837eeb5975fd3940b30c8b6b22b0c5f7d03afb
jonathanfeatherston <jonathan@esper.io>
2020-11-25 16:08:11 -0800

    Adjust no config file found. Remove unused.

--- Common/Globals.py
+++ Common/Globals.py
@@ -6,7 +6,6 @@ enterprise_id = ""
 """ Constants """
 VERSION = "0.1"
 TITLE = "Esper API Support Tool"
-CONFIGFILE = "EsperGroupActionsConfig.csv"
 limit = 500000  # int | Number of results to return per page. (optional) (default to 20)
 offset = 0  # int | The initial index from which to return the results. (optional) (default to 0)
 SHOW_GRID_DIALOG = True
