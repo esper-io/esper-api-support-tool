f54293daeb9ec288db65ca4a498d01bed4f56f30
jonathanfeatherston <jonathan@esper.io>
2021-03-01 16:50:30 -0800

    reorder

--- Common/Globals.py
+++ Common/Globals.py
@@ -14,29 +14,16 @@ MAX_LIMIT = 500000
 MAX_UPDATE_COUNT = 500
 MIN_SIZE = (900, 700)
 lock = threading.Lock()
-limit = (
-    MAX_LIMIT  # int | Number of results to return per page. (optional) (default to 20)
-)
-offset = 0  # int | The initial index from which to return the results. (optional) (default to 0)
-SHOW_GRID_DIALOG = True
-SHOW_TEMPLATE_DIALOG = True
-SHOW_TEMPLATE_UPDATE = True
+
 MAX_THREAD_COUNT = 8
 MAX_RETRY = 5
 MAX_STATUS_CHAR = 80
 PRINT_RESPONSES = False
 PRINT_FUNC_DURATION = False
 
-
 DESCRIPTION = """Esper API Support Tool makes use of Esper's APIs to programmatically control and monitor 
 your enterprise's Android-based Dedicated Devices providing features that are not currently
 available on the Esper Console Dashboard."""
-
-ESPER_LINK = "https://esper.io/"
-HELP_LINK = "https://docs.google.com/document/d/1WwDIQ-7CzQscVNFhiErbYtIwMyE34hGxE_MQWBqc9_k/edit#heading=h.50j8ygvoempc"
-UPDATE_LINK = (
-    "https://api.github.com/repos/esper-io/esper-api-support-tool/releases/latest"
-)
 
 """ Actions """
 GENERAL_ACTIONS = [
@@ -89,6 +76,11 @@ COMMAND_TYPES = [
 ]
 
 """ URL Requests and Extensions """
+ESPER_LINK = "https://esper.io/"
+HELP_LINK = "https://docs.google.com/document/d/1WwDIQ-7CzQscVNFhiErbYtIwMyE34hGxE_MQWBqc9_k/edit#heading=h.50j8ygvoempc"
+UPDATE_LINK = (
+    "https://api.github.com/repos/esper-io/esper-api-support-tool/releases/latest"
+)
 BASE_REQUEST_URL = "{configuration_host}/enterprise/{enterprise_id}/"
 BASE_DEVICE_URL = BASE_REQUEST_URL + "device/{device_id}/"
 BASE_REQUEST_EXTENSION = "/?&format=json"
@@ -152,3 +144,10 @@ USE_ENTERPRISE_APP = True
 SHOW_PKG_NAME = False
 REACH_QUEUED_ONLY = False
 GET_APP_EACH_DEVICE = False
+SHOW_GRID_DIALOG = True
+SHOW_TEMPLATE_DIALOG = True
+SHOW_TEMPLATE_UPDATE = True
+limit = (
+    MAX_LIMIT  # int | Number of results to return per page. (optional) (default to 20)
+)
+offset = 0  # int | The initial index from which to return the results. (optional) (default to 0)
