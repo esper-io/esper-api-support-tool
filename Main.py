229b8476d7315175edeed90bc22f3a19ec9b0a76
jonathanfeatherston <jonathan@esper.io>
2021-05-17 15:31:03 -0700

    add some start-up args

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@
 from GUI.WXFrameLayoutNew import NewFrameLayout as FrameLayout
 from Utility.ApiToolLogging import ApiToolLog
 
+import argparse
 import Common.Globals as Globals
 import sys
 import wx
@@ -41,5 +42,44 @@ def main():
         ApiToolLog().LogError(e)
 
 
+def parseArgs():
+    parser = argparse.ArgumentParser(description=Globals.DESCRIPTION)
+    parser.add_argument(
+        "--print_responses",
+        dest="print_res",
+        action="store",
+        nargs="?",
+        default=False,
+        help="Print Responses",
+        required=False,
+    )
+    parser.add_argument(
+        "--print_duration",
+        dest="print_duration",
+        action="store",
+        nargs="?",
+        default=False,
+        help="Print Duration of Methods",
+        required=False,
+    )
+    parser.add_argument(
+        "--record_place",
+        dest="record_place",
+        action="store",
+        nargs="?",
+        default=False,
+        help="Record Execution Pathing",
+        required=False,
+    )
+    args = parser.parse_args()
+    if hasattr(args, "record_place"):
+        Globals.RECORD_PLACE = args.record_place
+    if hasattr(args, "print_res"):
+        Globals.PRINT_RESPONSES = args.print_res
+    if hasattr(args, "print_duration"):
+        Globals.PRINT_FUNC_DURATION = args.print_duration
+
+
 if __name__ == "__main__":
+    parseArgs()
     main()
