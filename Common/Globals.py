e4f9eed8b1180b6e849b23465b9f90a6a9e92741
jonathanfeatherston <jonathan@esper.io>
2021-06-02 09:18:23 -0700

    increment

--- Common/Globals.py
+++ Common/Globals.py
@@ -9,7 +9,7 @@ configuration = esperclient.Configuration()
 enterprise_id = ""
 
 """ Constants """
-VERSION = "v0.184"
+VERSION = "v0.185"
 TITLE = "Esper API Support Tool"
 RECORD_PLACE = False
 MIN_LIMIT = 50
