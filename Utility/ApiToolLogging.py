7a2b3d8e61376f8b24d136e3c86b5500b552f177
jonathanfeatherston <jonathan@esper.io>
2021-10-14 08:24:24 -0700

    fix error preventing script from running

--- Utility/ApiToolLogging.py
+++ Utility/ApiToolLogging.py
@@ -50,7 +50,7 @@ class ApiToolLog:
         self.limitFileSize(self.placePath)
 
     def limitFileSize(self, file, maxFileSizeInMb=5):
-        if (os.path.getsize(file) / (1024 * 1024)) > maxFileSizeInMb:
+        if os.path.exists(file) and (os.path.getsize(file) / (1024 * 1024)) > maxFileSizeInMb:
             with open(file, "w"):
                 pass
 
