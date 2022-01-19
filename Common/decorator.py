ae5a0f78b7f7b512c7abb05020ab13b28bf621e6
Jonathan Featherston <jonathan@esper.io>
2022-01-19 11:48:10 -0800

    ensure lock is released

--- Common/decorator.py
+++ Common/decorator.py
@@ -110,12 +110,16 @@ def displayApiExcpetionMsg(e):
         "%s %s: %s" % (e.reason, e.status, bodyMsg),
         style=wx.OK | wx.ICON_ERROR,
     )
+    if Globals.msg_lock.locked():
+        Globals.msg_lock.release()
     return e
 
 
 def displayGenericErrorMsg(e):
     Globals.msg_lock.acquire(timeout=10)
     wx.MessageBox("An Error has occured: \n\n%s" % e, style=wx.OK | wx.ICON_ERROR)
+    if Globals.msg_lock.locked():
+        Globals.msg_lock.release()
     return e
 
 
