b18a216a14304ef679bdb826eedf9496ee9f786e
jonathanfeatherston <jonathan@esper.io>
2022-04-08 14:53:30 -0700

    formatting

--- Utility/EastUtility.py
+++ Utility/EastUtility.py
@@ -135,7 +135,7 @@ def iterateThroughDeviceList(frame, action, api_response, entId):
         action == GeneralActions.GENERATE_APP_REPORT.value
         or action == GeneralActions.SHOW_ALL_AND_GENERATE_REPORT.value
         or "Applications" in Globals.CSV_TAG_ATTR_NAME.keys()
-    )  and action != GeneralActions.GENERATE_DEVICE_REPORT.value
+    ) and action != GeneralActions.GENERATE_DEVICE_REPORT.value
     getLatestEvents = (
         action == GeneralActions.GENERATE_INFO_REPORT.value
         or action == GeneralActions.SHOW_ALL_AND_GENERATE_REPORT.value
