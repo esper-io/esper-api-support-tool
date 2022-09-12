70739d0d8c5c77d1071abfe1491b250501139be7
jonathanfeatherston <jonathan@esper.io>
2022-09-12 13:01:01 -0700

    add additional information to file content

--- Utility/API/ContentUtility.py
+++ Utility/API/ContentUtility.py
@@ -35,11 +35,16 @@ def uploadContentToHost(host, enterprise, key, file):
     url = "{baseUrl}/v0/enterprise/{enterprise_id}/content/upload/".format(
         baseUrl=host, enterprise_id=enterprise
     )
+    file = {
+        "Content-Disposition": 'form-data; name="key"; filename="%s"' % file,
+        "key": open(file, "rb"),
+        "Content-Type": "application/json"
+    }
     resp = performPostRequestWithRetry(
         url,
         headers={
             "Authorization": "Bearer %s" % key,
         },
-        files={"key": open(file, "rb")},
+        files=file,
     )
     return resp
