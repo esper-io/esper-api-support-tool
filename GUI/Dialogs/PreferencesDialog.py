4ae997807ed5f4afb22e3fb8c4c99d286edd309a
jonathanfeatherston <jonathan@esper.io>
2022-10-07 14:56:39 -0700

    formatting

--- GUI/Dialogs/PreferencesDialog.py
+++ GUI/Dialogs/PreferencesDialog.py
@@ -289,7 +289,9 @@ class PreferencesDialog(wx.Dialog):
             sizer_12,
             "Maximum Split Sheet Size",
             wx.SpinCtrl,
-            "Most Spreadsheet programs have issues display large amounts of data.\nThis preference specifies the max amount of rows saved to a sheet.\nWill use Spinner value * 1000. Default: 100 -> 100,000 Max: 500 -> 500,000 Min: 50 -> 50,000",
+            "Most Spreadsheet programs have issues display large amounts of data."
+            + "\nThis preference specifies the max amount of rows saved to a sheet."
+            + "\nWill use Spinner value * 1000. Default: 100 -> 100,000 Max: 500 -> 500,000 Min: 50 -> 50,000",
         )
         self.spin_ctrl_12.SetMin(Globals.MIN_SHEET_CHUNK_SIZE / 1000)
         self.spin_ctrl_12.SetMax(Globals.MAX_SHEET_CHUNK_SIZE / 1000)
