ed9cde770532bd5b3c81f5953fe9124b4aaa07a5
jonathanfeatherston <jonathan@esper.io>
2023-09-26 12:21:32 -0700

    As apps have more row entries compared to device rows reduce the max count to enforce file save and avoid crash.

--- GUI/WXFrameLayoutNew.py
+++ GUI/WXFrameLayoutNew.py
@@ -2096,8 +2096,11 @@ class NewFrameLayout(wx.Frame):
                             ]
 
         if (
-            actionClientData <= GeneralActions.GENERATE_APP_REPORT.value
+            actionClientData < GeneralActions.GENERATE_APP_REPORT.value
             and estimatedDeviceCount > Globals.MAX_DEVICE_COUNT
+        ) or (
+            actionClientData == GeneralActions.GENERATE_APP_REPORT.value
+            and estimatedDeviceCount > (Globals.MAX_DEVICE_COUNT / 25)
         ):
             res = displayMessageBox(
                 (
