66af58dad91b51664a359c0fe1d51ada483374e6
jonathanfeatherston <jonathan@esper.io>
2023-10-18 15:48:13 -0700

    Remove unused var. Remove index column.

--- GUI/GridDataTable.py
+++ GUI/GridDataTable.py
@@ -7,7 +7,6 @@ import wx.grid
 class GridDataTable(wx.grid.GridTableBase):
     def __init__(self, data=None):
         wx.grid.GridTableBase.__init__(self)
-        self.headerRows = 1
         if data is None:
             data = pd.DataFrame()
         self.data = data
@@ -16,27 +15,20 @@ class GridDataTable(wx.grid.GridTableBase):
         return len(self.data)
 
     def GetNumberCols(self):
-        return len(self.data.columns)  # + 1 # for index
+        return len(self.data.columns)
 
     def GetValue(self, row, col):
-        if col == 0:
-            return self.data.index[row]
-        return self.data.iloc[row, col - 1]
+        return self.data.iloc[row, col]
 
     def SetValue(self, row, col, value):
-        self.data.iloc[row, col - 1] = value
+        self.data.iloc[row, col] = value
 
     def GetColLabelValue(self, col):
-        if col == 0:
-            if self.data.index.name is None:
-                return "Index"
-            else:
-                return self.data.index.name
-        return str(self.data.columns[col - 1])
+        return str(self.data.columns[col])
 
     def GetTypeName(self, row, col):
         if self.data[self.data.columns[0]].count() > 0:
-            value = self.data.iloc[row, col - 1]
+            value = self.data.iloc[row, col]
             if type(value) is int:
                 return wx.grid.GRID_VALUE_NUMBER
         return wx.grid.GRID_VALUE_STRING
