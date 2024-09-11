a2101bb099174680c7d24637cebf9fa829308ce2
jonathanfeatherston <jonathan@esper.io>
2024-09-11 10:23:26 -0700

    add condition to avoid possible error

--- GUI/WXFrameLayoutNew.py
+++ GUI/WXFrameLayoutNew.py
@@ -45,11 +45,14 @@ from GUI.sidePanel import SidePanel
 from GUI.toolBar import ToolsToolBar
 from Utility.API.AppUtilities import getAllInstallableApps, getAppDictEntry
 from Utility.API.AuditPosting import AuditPosting
-from Utility.API.BlueprintUtility import (checkFeatureFlags, getAllBlueprints,
-                                          modifyAppsInBlueprints,
-                                          prepareBlueprintClone,
-                                          prepareBlueprintConversion,
-                                          pushBlueprintUpdate)
+from Utility.API.BlueprintUtility import (
+    checkFeatureFlags,
+    getAllBlueprints,
+    modifyAppsInBlueprints,
+    prepareBlueprintClone,
+    prepareBlueprintConversion,
+    pushBlueprintUpdate,
+)
 from Utility.API.CommandUtility import createCommand, sendPowerDownCommand
 from Utility.API.DeviceUtility import getAllDevices
 from Utility.API.EsperAPICalls import getCompanySettings, validateConfiguration
@@ -57,27 +60,47 @@ from Utility.API.GroupUtility import getAllGroups, moveGroup
 from Utility.API.UserUtility import getAllPendingUsers, getAllUsers
 from Utility.API.WidgetUtility import setWidget
 from Utility.crypto import crypto
-from Utility.EastUtility import (TakeAction, clearKnownGlobalVariables,
-                                 fetchInstalledDevices, filterDeviceList,
-                                 getAllDeviceInfo, removeNonWhitelisted,
-                                 uploadAppToEndpoint)
-from Utility.FileUtility import (getToolDataPath, read_csv_via_pandas,
-                                 read_data_from_csv,
-                                 read_data_from_csv_as_dict,
-                                 read_excel_via_openpyxl, read_json_file,
-                                 save_csv_pandas, save_excel_pandas_xlxswriter,
-                                 write_data_to_csv, write_json_file)
+from Utility.EastUtility import (
+    TakeAction,
+    clearKnownGlobalVariables,
+    fetchInstalledDevices,
+    filterDeviceList,
+    getAllDeviceInfo,
+    removeNonWhitelisted,
+    uploadAppToEndpoint,
+)
+from Utility.FileUtility import (
+    getToolDataPath,
+    read_csv_via_pandas,
+    read_data_from_csv,
+    read_data_from_csv_as_dict,
+    read_excel_via_openpyxl,
+    read_json_file,
+    save_csv_pandas,
+    save_excel_pandas_xlxswriter,
+    write_data_to_csv,
+    write_json_file,
+)
 from Utility.GridActionUtility import iterateThroughGridRows
 from Utility.GridUtilities import createDataFrameFromDict, split_dataframe
 from Utility.Logging.ApiToolLogging import ApiToolLog
-from Utility.Resource import (checkEsperInternetConnection,
-                              checkForInternetAccess,
-                              checkIfCurrentThreadStopped, correctSaveFileName,
-                              createNewFile, determineDoHereorMainThread,
-                              displayFileDialog, displayMessageBox,
-                              joinThreadList, openWebLinkInBrowser,
-                              postEventToFrame, processFunc, resourcePath,
-                              splitListIntoChunks, updateErrorTracker)
+from Utility.Resource import (
+    checkEsperInternetConnection,
+    checkForInternetAccess,
+    checkIfCurrentThreadStopped,
+    correctSaveFileName,
+    createNewFile,
+    determineDoHereorMainThread,
+    displayFileDialog,
+    displayMessageBox,
+    joinThreadList,
+    openWebLinkInBrowser,
+    postEventToFrame,
+    processFunc,
+    resourcePath,
+    splitListIntoChunks,
+    updateErrorTracker,
+)
 
 
 class NewFrameLayout(wx.Frame):
@@ -2034,15 +2057,20 @@ class NewFrameLayout(wx.Frame):
     def setStatus(self, status, orgingalMsg, isError=False):
         """Set status bar text"""
         try:
-            self.statusBar.sbText.SetLabel(status)
-            if orgingalMsg:
-                self.statusBar.sbText.SetToolTip(
-                    orgingalMsg.replace("--->", "")
-                )
-            if isError:
-                self.statusBar.sbText.SetForegroundColour(Color.red.value)
-            else:
-                self.statusBar.sbText.SetForegroundColour(Color.black.value)
+            if (
+                self.statusBar
+                and hasattr(self.statusBar, "sbText")
+                and self.statusBar.sbText
+            ):
+                self.statusBar.sbText.SetLabel(status)
+                if orgingalMsg:
+                    self.statusBar.sbText.SetToolTip(
+                        orgingalMsg.replace("--->", "")
+                    )
+                if isError:
+                    self.statusBar.sbText.SetForegroundColour(Color.red.value)
+                else:
+                    self.statusBar.sbText.SetForegroundColour(Color.black.value)
         except Exception as e:
             ApiToolLog().LogError(e)
 
