c40d82dc79270c63fd2746c4bf9f367f9ca47bfe
jonathanfeatherston <jonathan@esper.io>
2025-03-06 12:48:49 -0800

    remove windows req

--- GUI/WXFrameLayoutNew.py
+++ GUI/WXFrameLayoutNew.py
@@ -2097,7 +2097,7 @@ class NewFrameLayout(wx.Frame):
                     )
                 if isError:
                     self.statusBar.sbText.SetForegroundColour(Color.red.value)
-                elif (wx.SystemSettings.GetAppearance().IsDark()) and self.WINDOWS:
+                elif (wx.SystemSettings.GetAppearance().IsDark()):
                     self.statusBar.sbText.SetForegroundColour(Color.white.value)
                 else:
                     self.statusBar.sbText.SetForegroundColour(Color.black.value)
