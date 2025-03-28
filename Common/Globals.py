93b76d35d040d6107b231c145e5b216b62385bbc
jonathanfeatherston <jonathan@esper.io>
2025-03-28 15:34:34 -0700

    increment version

--- Common/Globals.py
+++ Common/Globals.py
@@ -18,7 +18,7 @@ DO_EXTRA_LOGGING = True
 API_LOGGER = None
 
 """ Constants """
-VERSION = "v0.195264"
+VERSION = "v0.195265"
 TITLE = "Esper API Support Tool"
 RECORD_PLACE = False
 MIN_LIMIT = 50
