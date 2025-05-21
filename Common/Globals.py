1f70dd85da11473d0ef0bb339d539c4244c21198
jonathanfeatherston <jonathan@esper.io>
2025-05-21 10:30:15 -0700

    forgot file

--- Common/Globals.py
+++ Common/Globals.py
@@ -18,7 +18,7 @@ DO_EXTRA_LOGGING = True
 API_LOGGER = None
 
 """ Constants """
-VERSION = "v0.195271"
+VERSION = "v0.195272"
 TITLE = "Esper API Support Tool"
 RECORD_PLACE = False
 MIN_LIMIT = 50
